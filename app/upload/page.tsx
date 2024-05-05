75b4ebb522ba90a107374cd504564250d05e0741
Michael Prestonise <michael.prestonise@gmail.com>
2024-05-05 12:34:14 +0100

    Route to 'upload' on successful login

---
+++ app/upload/page.tsx
@@ -0,0 +1,44 @@
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+
+export default async function Account() {
+  const supabase = createClient();
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  const { data: userDetails } = await supabase
+    .from('users')
+    .select('*')
+    .single();
+
+  const { data: subscription, error } = await supabase
+    .from('subscriptions')
+    .select('*, prices(*, products(*))')
+    .in('status', ['trialing', 'active'])
+    .maybeSingle();
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (!user) {
+    return redirect('/signin');
+  }
+
+  return (
+    <section className="mb-32 bg-black">
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
+        <div className="sm:align-center sm:flex sm:flex-col">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+            Ready to post?
+          </h1>
+          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+            Upload your photo for Quippy to write a caption for
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
