16838561c21a5a4d9390f5fece44a964125d91b0
Michael Prestonise <michael.prestonise@gmail.com>
2024-05-05 12:17:07 +0100

    Updating meta and the account page copy

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -38,7 +38,7 @@ export default async function Account() {
             Account
           </h1>
           <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            We partnered with Stripe for a simplified billing.
+            Quippy uses Stripe to make it easy to manage your plan
           </p>
         </div>
       </div>
