d421eee0715dd458c246232f195474cdd1823b3e
Michael Prestonise <michael.prestonise@gmail.com>
2024-05-06 11:50:58 +0100

    Adding mock image

--- components/ui/Claude/Claude.tsx
+++ components/ui/Claude/Claude.tsx
@@ -4,17 +4,18 @@ import Button from '@/components/ui/Button';
 import { useState } from 'react';
 import Anthropic from '@anthropic-ai/sdk';
 
-
 export default function Claude() {
     const [isSubmitting, setIsSubmitting] = useState(false);
     let caption;
     const handleClaudeRequest = async () => {
         setIsSubmitting(true);
         const anthropic = new Anthropic();
-        const image_url = "https://i.etsystatic.com/18298644/r/il/785cfa/5510810648/il_1140xN.5510810648_flu0.jpg"
+        // const image_url = "https://i.etsystatic.com/18298644/r/il/785cfa/5510810648/il_1140xN.5510810648_flu0.jpg"
         const image_media_type = "image/jpeg"
-        const image_array_buffer = await ((await fetch(image_url)).arrayBuffer());
-        const image_data = Buffer.from(image_array_buffer).toString('base64');
+        // const image_array_buffer = await ((await fetch(image_url)).arrayBuffer());
+        // const image_data = Buffer.from(image_array_buffer).toString('base64');
+
+        const image_data = Buffer.from('/statement-trapezoid.jpg').toString('base64');
     
         const generatedCaption = await anthropic.messages.create({
           model: 'claude-3-opus-20240229',
