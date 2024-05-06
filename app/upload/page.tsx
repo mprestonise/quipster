8e15ec07a0531ba305740dcd82b17e41e5339fb6
Michael Prestonise <michael.prestonise@gmail.com>
2024-05-06 11:38:04 +0100

    Move Claude out of Upload component

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,15 +1,9 @@
-'use client';
-
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
-import Button from '@/components/ui/Button';
-import Anthropic from '@anthropic-ai/sdk';
-import { useState } from 'react';
+import Claude from '@/components/ui/Claude/Claude';
 
 export default async function Upload() {
   const supabase = createClient();
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  let caption;
 
   const {
     data: { user }
@@ -33,40 +27,7 @@ export default async function Upload() {
     return redirect('/');
   }
 
-  const handleClaudeRequest = async () => {
-    setIsSubmitting(true);
-    const anthropic = new Anthropic();
-    const image_url = "https://i.etsystatic.com/18298644/r/il/785cfa/5510810648/il_1140xN.5510810648_flu0.jpg"
-    const image_media_type = "image/jpeg"
-    const image_array_buffer = await ((await fetch(image_url)).arrayBuffer());
-    const image_data = Buffer.from(image_array_buffer).toString('base64');
-
-    const generatedCaption = await anthropic.messages.create({
-      model: 'claude-3-opus-20240229',
-      max_tokens: 1024,
-      messages: [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image",
-                        "source": {
-                            "type": "base64",
-                            "media_type": image_media_type,
-                            "data": image_data,
-                        },
-                    },
-                    {
-                      "type": "text",
-                      "text": "The client wants you to write a witty, friendly, and quirky caption for this photo that they can use when publishing this photo to Instagram"
-                  }
-                ],
-            }
-          ]
-    });
-    setIsSubmitting(false);
-    caption = generatedCaption;
-  };
+  
 
   return (
     <section className="mb-32 bg-black">
@@ -81,15 +42,7 @@ export default async function Upload() {
           <h3>Let's start with a dummy photo and known brand settings</h3>
           <img src="https://i.etsystatic.com/18298644/r/il/785cfa/5510810648/il_1140xN.5510810648_flu0.jpg" alt="Statement Trapezoids" height="100" />
           <p>Brand settings: Witty, Friendly, and Quirky</p>
-          <Button
-            variant="slim"
-            onClick={handleClaudeRequest}
-            loading={isSubmitting}
-            disabled={isSubmitting}
-          >
-            Get caption
-          </Button>
-          {caption ? <p>Caption: {caption}</p> : <p>Click the button above to generate a caption</p>}
+          <Claude />
         </div>
       </div>
     </section>
---
+++ components/ui/Claude/Claude.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import Button from '@/components/ui/Button';
+import { useState } from 'react';
+import Anthropic from '@anthropic-ai/sdk';
+
+
+export default function Claude() {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    let caption;
+    const handleClaudeRequest = async () => {
+        setIsSubmitting(true);
+        const anthropic = new Anthropic();
+        const image_url = "https://i.etsystatic.com/18298644/r/il/785cfa/5510810648/il_1140xN.5510810648_flu0.jpg"
+        const image_media_type = "image/jpeg"
+        const image_array_buffer = await ((await fetch(image_url)).arrayBuffer());
+        const image_data = Buffer.from(image_array_buffer).toString('base64');
+    
+        const generatedCaption = await anthropic.messages.create({
+          model: 'claude-3-opus-20240229',
+          max_tokens: 1024,
+          messages: [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image",
+                            "source": {
+                                "type": "base64",
+                                "media_type": image_media_type,
+                                "data": image_data,
+                            },
+                        },
+                        {
+                          "type": "text",
+                          "text": "The client wants you to write a witty, friendly, and quirky caption for this photo that they can use when publishing this photo to Instagram"
+                      }
+                    ],
+                }
+              ]
+        });
+        setIsSubmitting(false);
+        caption = generatedCaption;
+      };
+
+    return (
+    <div>
+        <Button
+        variant="slim"
+        onClick={handleClaudeRequest}
+        loading={isSubmitting}
+        disabled={isSubmitting}
+      >
+        Get caption
+      </Button>
+      {caption ? <p>Caption: {caption}</p> : <p>Click the button above to generate a caption</p>}
+    </div>
+    );
+  }
