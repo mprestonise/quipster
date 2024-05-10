316615b19088b163a8c898c05a9fad354bb55f5b
Michael Prestonise <michael.prestonise@gmail.com>
2024-05-10 05:37:56 +0100

    getting userid in upload client

--- utils/photos/client.ts
+++ utils/photos/client.ts
@@ -28,9 +28,9 @@ export async function uploadPhoto(e: React.FormEvent<HTMLFormElement>, userid: R
   const formData = new FormData(e.currentTarget);
   let photo = formData.get('photo')!;
 
-  console.log("Okay, what is userid?", userid);
+  console.log("Okay, what is userid.userid?", userid?.userid);
 
-  const { data, error } = await supabase.storage.from('photos').upload(userid?.id + '/' + uuid(), photo, {
+  const { data, error } = await supabase.storage.from('photos').upload(userid?.userid + '/' + uuid(), photo, {
     cacheControl: '3600',
     upsert: false
   }) as unknown as { data: UploadResponse; error: UploadResponse };
